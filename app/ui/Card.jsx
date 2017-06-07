530159808d2a06ed54213a28675f825b6041e852
Daniel Kang <dkinsk@gmail.com>
2017-06-07 17:30:48 -0400

    changes made

--- app/ui/Card.jsx
+++ app/ui/Card.jsx
@@ -4,11 +4,10 @@ import classNames from 'classnames';
 export default class CardComponent extends React.Component {
 	render() {
 		const children = React.Children.map(this.props.children, child => {
-			 React.cloneElement(child));
-	}
+			return React.cloneElement(child);
+		});
 
 		let props = Object.assign({}, this.props);
-
 		props.className = classNames('card', this.props.className);
 
 		return React.createElement('div', props, children);
