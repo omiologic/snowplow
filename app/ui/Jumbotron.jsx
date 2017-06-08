a72e4c971e4643d32ad94fa80666edfe32a9b10f
Daniel Kang <dkinsk@gmail.com>
2017-06-07 20:53:45 -0400

    resubmission

--- app/ui/Jumbotron.jsx
+++ app/ui/Jumbotron.jsx
@@ -3,9 +3,9 @@ import classNames from 'classnames';
 
 export default class JumbotronComponent extends React.Component {
 	render() {
-		const children = React.Children.map(this.props.children, (child) => {
-				return React.cloneElement(child))
-		};
+		const children = React.Children.map(this.props.children, child => {
+			return React.cloneElement(child);
+		});
 
 		let props = Object.assign({}, this.props);
 
