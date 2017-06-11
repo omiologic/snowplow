20e7f7efa63463e346c452ea28f45a5acbc7fe58
milocosmopolitan <milocosmopolitan@gmail.com>
2017-06-11 19:57:20 -0400

    Section Component impemented

--- app/containers/Home.jsx
+++ app/containers/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Jumbotron from '../ui/Jumbotron';
-import About from '../components/About';
 import Team from '../components/Team';
 import Service from '../components/Service/Service';
 
